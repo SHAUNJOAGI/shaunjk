127a997b8fd44af03b903d0cfe8c6e9fb4f9bf76
SHAUNJOAGI <134275848+SHAUNJOAGI@users.noreply.github.com>
2023-05-22 15:08:33 +0530

    Add files via upload
    internship first day

---
+++ New folder/array.js
@@ -0,0 +1,11 @@
+let classess=[{
+    name:"Tiya",
+    age:20,
+    place:"koratty",
+}
+{
+    name:"Maya",
+    age:10,
+    place:"EKM"
+}];
+console.log(classess)
---
+++ New folder/script.js
@@ -0,0 +1,15 @@
+let fruits =["applee","GRAPES","WAtermelon",11];
+fruits.push("banana")
+console.log(fruits);
+fruits.pop();
+console.log(fruits)
+fruits.unshift("xxx")
+console.log(fruits);
+fruits.shift();
+console.log(fruits[0]);
+
+
+
+
+for(let i=1,i<n,i++),
+console.log(i);74
